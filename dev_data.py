aef8039c393a365852224352542f450a1248f5e0
Tim <6698857+tims457@users.noreply.github.com>
2025-03-17 21:13:11 -0600

    init and data

---
+++ dev_data.py
@@ -0,0 +1,17 @@
+# from datasets import load_dataset
+# # use name="sample-10BT" to use the 10BT sample
+# fw = load_dataset("HuggingFaceFW/fineweb", name="CC-MAIN-2024-10", split="train", streaming=True)
+
+
+# print(fw.take(10))
+
+
+from datatrove.pipeline.readers import ParquetReader
+
+# limit determines how many documents will be streamed (remove for all)
+# to fetch a specific dump: hf://datasets/HuggingFaceFW/fineweb/data/CC-MAIN-2024-10
+# replace "data" with "sample/100BT" to use the 100BT sample
+data_reader = ParquetReader("hf://datasets/HuggingFaceFW/fineweb/data", limit=10) 
+for document in data_reader():
+    # do something with document
+    print(document)
